1e52abc8dc425dd24f3ff7045a54b14f3952620a
Olowerr <oliver.klingspor@hotmail.com>
2023-07-01 18:44:43 +0200

    Added camera movement
    Also restructured imgui windows

--- source/Application/Application.cpp
+++ source/Application/Application.cpp
@@ -5,6 +5,11 @@
 
 #include "ImGuiHelper.h"
 
+#include "Input.h"
+
+#include "glm/gtx/quaternion.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 Application::Application()
 	:m_pBackBuffer(nullptr)
 {
@@ -16,6 +21,7 @@ Application::Application()
 	Okay::initiateDX11();
 	m_window.initiate(1600u, 900u, "GPU Raytracer");
 	m_renderer.initiate(m_window.getBackBuffer(), &m_scene);
+	m_renderer.toggleAccumulation(true);
 
 	Okay::initiateImGui(m_window.getGLFWWindow());
 	Okay::getDevice()->CreateRenderTargetView(m_window.getBackBuffer(), nullptr, &m_pBackBuffer);
@@ -36,9 +42,9 @@ void Application::run()
 {
 	static ID3D11RenderTargetView* nullRTV = nullptr;
 
-	Entity camEntity = m_scene.createEntity();
-	camEntity.addComponent<Camera>(90.f, 0.1f);
-	m_renderer.setCamera(camEntity);
+	m_camera = m_scene.createEntity();
+	m_camera.addComponent<Camera>(90.f, 0.1f);
+	m_renderer.setCamera(m_camera);
 
 	SphereComponent& sun = m_scene.createEntity().addComponent<SphereComponent>();
 	sun.position = glm::vec3(325.f, 283.f, 20.f);
@@ -67,35 +73,32 @@ void Application::run()
 		m_window.processMessages();
 		Okay::newFrameImGui();
 
-		if (ImGui::Begin("Spheres"))
-		{
-			ImGui::PushItemWidth(-120.f);
+		static bool accumulate = true;
+		m_accumulationTime += ImGui::GetIO().DeltaTime;
+		m_accumulationTime *= (float)accumulate;
 
+		if (ImGui::Begin("Data"))
+		{
 			ImGui::Text("FPS: %.3f", 1.f / ImGui::GetIO().DeltaTime);
 			ImGui::Text("MS: %.4f", ImGui::GetIO().DeltaTime * 1000.f);
 
 			ImGui::Separator();
 
-			Camera& camera = camEntity.getComponent<Camera>();
-			ImGui::PushID(camEntity.getID());
-
-			ImGui::Text("Camera");
-			ImGui::DragFloat3("Position", &camera.position.x, 0.1f);
-			ImGui::DragFloat3("Rotation", &camera.rotation.x, 0.1f);
-			ImGui::DragFloat("FOV", &camera.fov, 0.5f);
-			ImGui::DragFloat("NearZ", &camera.nearZ, 0.001f);
-			ImGui::PopID();
-
-			ImGui::Separator();
-
-			static bool accumulate = false;
-			ImGui::Text("Num Accumulation Frames: %u", m_renderer.getNumAccumulationFrames());
+			ImGui::Text("Accumulation Frames: %u", m_renderer.getNumAccumulationFrames());
+			ImGui::Text("Accumulation Time:	%.2f", m_accumulationTime);
 			if (ImGui::Checkbox("Accumulate", &accumulate))
 				m_renderer.toggleAccumulation(accumulate);
 			if (ImGui::Button("Reset Accumulation"))
+			{
 				m_renderer.resetAccumulation();
+				m_accumulationTime = 0.f;
+			}
+		}
 
-			ImGui::Separator();
+
+		if (ImGui::Begin("Spheres"))
+		{
+			ImGui::PushItemWidth(-120.f);
 
 			if (ImGui::Button("Add Sphere"))
 				m_scene.createEntity().addComponent<SphereComponent>();
@@ -125,6 +128,7 @@ void Application::run()
 		}
 		ImGui::End();
 
+		updateCamera();
 		m_renderer.render();
 
 		Okay::getDeviceContext()->OMSetRenderTargets(1u, &m_pBackBuffer, nullptr);
@@ -132,5 +136,66 @@ void Application::run()
 		Okay::getDeviceContext()->OMSetRenderTargets(1u, &nullRTV, nullptr);
 
 		m_window.present();
+	}
+}
+
+void Application::updateCamera()
+{
+	static float rotationSpeed = 0.1f;
+	static float moveSpeed = 20.f;
+	Camera& cameraData = m_camera.getComponent<Camera>();
+
+	if (ImGui::Begin("Camera"))
+	{
+		ImGui::PushItemWidth(-120.f);
+
+		ImGui::Text("Pos: (%.3f, %.3f, %.3f)", cameraData.position.x, cameraData.position.y, cameraData.position.z);
+		ImGui::Text("Rot: (%.3f, %.3f, %.3f)", cameraData.rotation.x, cameraData.rotation.y, cameraData.rotation.z);
+
+		ImGui::Separator();
+
+		ImGui::DragFloat("Rotation Speed", &rotationSpeed, 0.02f);
+		ImGui::DragFloat("Move Speed", &moveSpeed, 0.2f);
+
+		ImGui::PopItemWidth();
+	}
+	ImGui::End();
+
+
+	static MouseMode mouseMode = MouseMode::Normal;
+	if (Input::isKeyPressed(Key::E))
+	{
+		mouseMode = mouseMode == MouseMode::Normal ? MouseMode::Locked : MouseMode::Normal;
+		Input::setMouseMode(mouseMode);
+	}
+
+	if (mouseMode == MouseMode::Normal)
+		return;
+
+	const glm::vec2 mouseDelta = Input::getMouseDelta();
+
+	cameraData.rotation.x += mouseDelta.y * rotationSpeed;
+	cameraData.rotation.y += mouseDelta.x * rotationSpeed;
+	cameraData.rotation.x = glm::clamp(cameraData.rotation.x, -89.f, 89.f);
+
+	const float xInput = (float)Input::isKeyDown(Key::D) - (float)Input::isKeyDown(Key::A);
+	const float yInput = (float)Input::isKeyDown(Key::Space) - (float)Input::isKeyDown(Key::LeftControl);
+	const float zInput = (float)Input::isKeyDown(Key::W) - (float)Input::isKeyDown(Key::S);
+
+	const glm::mat3 rotationMatrix = glm::toMat3(glm::quat(glm::radians(cameraData.rotation)));
+
+	const glm::vec3 fwd = rotationMatrix[2];
+	const glm::vec3 right = rotationMatrix[0];
+
+	const float frameSpeed = ImGui::GetIO().DeltaTime * moveSpeed * (Input::isKeyDown(Key::LeftShift) ? 3.f : 1.f);
+
+	cameraData.position += (right * xInput + fwd * zInput) * frameSpeed;
+	cameraData.position.y += yInput * frameSpeed;
+
+
+	if (xInput || yInput || zInput || mouseDelta.x || mouseDelta.y)
+	{
+		m_renderer.resetAccumulation();
+		m_accumulationTime = 0.f;
 	}
 }
--- source/Application/Application.h
+++ source/Application/Application.h
@@ -16,4 +16,9 @@ private:
 	Renderer m_renderer;
 	Scene m_scene;
 	ID3D11RenderTargetView* m_pBackBuffer; //Only used for ImGui
+
+	Entity m_camera;
+	void updateCamera();
+
+	float m_accumulationTime;
 };
