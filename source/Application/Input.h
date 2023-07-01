e3690d3b5a1fda2c7e41c084f0f7c203d124648a
Olowerr <oliver.klingspor@hotmail.com>
2023-07-01 17:53:34 +0200

    Added mouse button inputs

--- source/Application/Input.h
+++ source/Application/Input.h
@@ -14,6 +14,12 @@ enum MouseMode : int
 	Locked		= GLFW_CURSOR_DISABLED,
 };
 
+enum MouseButton : char
+{
+	Left		= 0,
+	Right		= 1,
+};
+
 class Input
 {
 public:
@@ -27,6 +33,10 @@ public:
 	static inline glm::vec2 getMousePos();
 	static inline glm::vec2 getMouseDelta();
 	static inline void setMouseMode(MouseMode mode);
+
+	static inline bool isMouseClicked(MouseButton button);
+	static inline bool isMouseDown(MouseButton button);
+	static inline bool isMouseReleased(MouseButton button);
 
 private:
 	// NOTE: Fixes mouse inputs temporarily, 'should' be window specific
@@ -42,24 +52,37 @@ private:
 	static inline glm::vec2 mousePos{};
 	static inline glm::vec2 prevMousePos{};
 
+	static inline bool mouseButtonsDown[2]{};
+	static inline bool prevMouseButtonsDown[2]{};
 };
 
-inline bool Input::isKeyDown(Key::KeyCode key)		{ return Input::keys[key]; }
-inline bool Input::isKeyPressed(Key::KeyCode key)	{ return Input::keys[key] && !Input::prevKeys[key]; }
-inline bool Input::isKeyReleased(Key::KeyCode key)	{ return !Input::keys[key] && Input::prevKeys[key]; }
+inline bool Input::isKeyDown(Key::KeyCode key)			{ return Input::keys[key]; }
+inline bool Input::isKeyPressed(Key::KeyCode key)		{ return Input::keys[key] && !Input::prevKeys[key]; }
+inline bool Input::isKeyReleased(Key::KeyCode key)		{ return !Input::keys[key] && Input::prevKeys[key]; }
 
-inline void Input::setKeyDown(Key::KeyCode key)		{ if (key >= Key::NUM_KEYS) return; keys[key] = true; }
-inline void Input::setKeyUp(Key::KeyCode key)		{ if (key >= Key::NUM_KEYS) return; keys[key] = false; }
+inline void Input::setKeyDown(Key::KeyCode key)			{ if (key >= Key::NUM_KEYS) return; keys[key] = true; }
+inline void Input::setKeyUp(Key::KeyCode key)			{ if (key >= Key::NUM_KEYS) return; keys[key] = false; }
 
-inline glm::vec2 Input::getMousePos()				{ return Input::mousePos; }
-inline glm::vec2 Input::getMouseDelta()				{ return Input::mousePos - Input::prevMousePos; }
-inline void Input::setMouseMode(MouseMode mode)		{ glfwSetInputMode(pWindow, GLFW_CURSOR, mode); }
+inline glm::vec2 Input::getMousePos()					{ return Input::mousePos; }
+inline glm::vec2 Input::getMouseDelta()					{ return Input::mousePos - Input::prevMousePos; }
+inline void Input::setMouseMode(MouseMode mode)			{ glfwSetInputMode(pWindow, GLFW_CURSOR, mode); }
+
+inline bool Input::isMouseClicked(MouseButton button)	{ return !prevMouseButtonsDown[button] && mouseButtonsDown[button]; }
+inline bool Input::isMouseDown(MouseButton button)		{ return mouseButtonsDown[button]; }
+inline bool Input::isMouseReleased(MouseButton button)	{ return prevMouseButtonsDown[button] && !mouseButtonsDown[button];	}
 
 inline void Input::update()
 {
 	// Keyboard inputs
 	memcpy(Input::prevKeys, Input::keys, size_t(Key::NUM_KEYS));
 	
+	// Mouse inputs
+	memcpy(prevMouseButtonsDown, mouseButtonsDown, sizeof(bool) * 2);
+	int leftState = glfwGetMouseButton(pWindow, GLFW_MOUSE_BUTTON_LEFT);
+	int rightState = glfwGetMouseButton(pWindow, GLFW_MOUSE_BUTTON_RIGHT);
+	mouseButtonsDown[0] = leftState == GLFW_PRESS;
+	mouseButtonsDown[1] = rightState == GLFW_PRESS;
+
 	// Mouse position
 	double xPos, yPos;
 	glfwGetCursorPos(pWindow, &xPos, &yPos);
